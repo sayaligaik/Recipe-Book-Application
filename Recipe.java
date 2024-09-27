0e0e9eab852ae327840e8c840e296ff65ce75743
Sayali Gaikwad <124872737+sayaligaik@users.noreply.github.com>
2024-09-27 21:06:26 +0530

    Recipe.java

---
+++ Recipe.java
@@ -0,0 +1,11 @@
+package com.recipebook;
+
+public class Recipe {
+    private int id;
+    private String title;
+    private String ingredients;
+    private String instructions;
+
+    // Getters and Setters
+    // Constructor
+}
